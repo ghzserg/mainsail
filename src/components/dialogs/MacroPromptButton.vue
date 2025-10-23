466d11888e3a78ff5ecbf07ca2261f3c9d1143ab
Sergei Rozhkov <fgup@zserg.ru>
2025-10-23 14:06:28 +0500

    Fix Prettier formatting in MacroPromptButton.vue

--- src/components/dialogs/MacroPromptButton.vue
+++ src/components/dialogs/MacroPromptButton.vue
@@ -28,46 +28,46 @@ export default class MacroPromptButton extends Mixins(BaseMixin) {
     }
     // https://github.com/function3d/mainsail/
     get bgcolor() {
-        return this.splits[3] ? '#'+this.splits[3] : ''
+        return this.splits[3] ? '#' + this.splits[3] : ''
     }
 
     get computedStyle() {
-        let styles = '';
+        let styles = ''
         if (this.bgcolor) {
-            styles += `background-color: ${this.bgcolor} !important; `;
+            styles += `background-color: ${this.bgcolor} !important; `
         }
         if (this.textcolor) {
-            styles += `color: ${this.textcolor} !important; `;
+            styles += `color: ${this.textcolor} !important; `
         }
-        return styles;
+        return styles
     }
 
     get textcolor() {
         if (this.bgcolor) {
             var hex = this.hexToRgb(this.bgcolor)
-            var gray = hex.r*0.299 + hex.g*0.587 + hex.b*0.114
-            if (gray > 186)
-                return 'black'
-            else
-                return 'white'
+            var gray = hex.r * 0.299 + hex.g * 0.587 + hex.b * 0.114
+            if (gray > 186) return 'black'
+            else return 'white'
         } else {
             return ''
         }
     }
 
     hexToRgb(hex) {
-      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-      hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-      });
+        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
+        hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+            return r + r + g + g + b + b
+        })
 
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-      } : null;
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+        return result
+            ? {
+                  r: parseInt(result[1], 16),
+                  g: parseInt(result[2], 16),
+                  b: parseInt(result[3], 16),
+              }
+            : null
     }
     // end https://github.com/function3d/mainsail/
     sendCommand() {
